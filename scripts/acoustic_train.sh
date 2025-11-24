2eaa0eb35abf245b065d9afa55b39606b0239e31
tysunseven <tysun@mail.ustc.edu.cn>
2025-11-24 15:07:28 +0800

    提升代码可读性

--- scripts/acoustic_train.sh
+++ scripts/acoustic_train.sh
@@ -8,7 +8,8 @@ CONTINUE_TRAINING=true
 # 仅在 CONTINUE_TRAINING=true 时使用:
 # 在这里填入您想要继续训练的那个文件夹的时间戳
 # 例如: 20251115142819
-LOAD_TIMESTAMP_ID="20251115170107" 
+LOAD_TIMESTAMP_ID="20251120082359" 
+# LOAD_TIMESTAMP_ID="20251120082359" 这个是目前训练了8000epoch的模型 
 # --- 结束配置 ---
 
 # --- DDP 安全逻辑 (自动) ---
@@ -25,4 +26,20 @@ echo "使用的时间戳 ID: $RUN_TIMESTAMP"
 # --- 结束逻辑 ---
 
 # 更改: 调用新的训练脚本 acoustic_train.py
-python3 acoustic_train.py --run_timestamp $RUN_TIMESTAMP --results_folder $RESULT_FOLDER --data_class microstructure --name model --batch_size 256 --continue_training $CONTINUE_TRAINING --image_size 128 --training_epoch 200 --ema_rate 0.999 --base_channels 32 --save_last True --save_every_epoch 50 --with_attention True  --split_dataset False  --lr 1e-4 --optimizier adamw --img_folder /root/autodl-fs
+python3 acoustic_train.py --run_timestamp $RUN_TIMESTAMP \
+                        --results_folder $RESULT_FOLDER \
+                        --data_class microstructure \
+                        --name model \
+                        --batch_size 256 \
+                        --continue_training $CONTINUE_TRAINING \
+                        --image_size 8 \
+                        --training_epoch 8020 \
+                        --ema_rate 0.999 \
+                        --base_channels 32 \
+                        --save_last True \
+                        --save_every_epoch 50 \
+                        --with_attention True  \
+                        --split_dataset False  \
+                        --lr 1e-4 \
+                        --optimizier adamw \
+                        --img_folder /root/autodl-fs/acoustic_dataset1
