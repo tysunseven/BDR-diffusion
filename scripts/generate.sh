cda1feac0da405d3a46f760439030e1b72052747
tysunseven <tysun@mail.ustc.edu.cn>
2025-11-24 15:07:56 +0800

    修改生成脚本的文件夹路径

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/python3 generate.py --step 50 --generate_method generate_based_on_bdr_json --model_path /data1/fjx/bdr-models/model8/model/epoch=1999.ckpt --output_path /data1/fjx/bdr-models/model8/output_target/output --bdr_path /home/fjx/bdr --json_path /home/fjx/bdr62/nsample.json 
+python3 generate.py --step 50 --generate_method generate_based_on_bdr_json --model_path /root/autodl-tmp/bdr_results/model_20251115170107/epoch=3999.ckpt --output_path /root/autodl-tmp/bdr_results/model_20251115170107/output --bdr_path /root/autodl-fs/img --json_path /root/autodl-fs/countT.json 
